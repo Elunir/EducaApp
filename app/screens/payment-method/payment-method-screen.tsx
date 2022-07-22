f74c7104e576cedda74e482180a5a5b284ce9412
Idris Attar <idrisattar@live.com>
2022-07-22 12:26:29 +0530

    submit

--- app/screens/payment-method/payment-method-screen.tsx
+++ app/screens/payment-method/payment-method-screen.tsx
@@ -1,20 +1,129 @@
-import React, { FC } from "react"
+/* eslint-disable react-native/no-color-literals */
+/* eslint-disable react/jsx-no-duplicate-props */
+/* eslint-disable react-native/no-inline-styles */
+import React, { FC, useState } from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { NavigatorParamList } from "../../navigators"
-import { Screen, Text } from "../../components"
-import { color } from "../../theme"
+import { AutoImage, Button, Screen, Text } from "../../components"
+import { TouchableOpacity, View } from "react-native"
+import Icon from "react-native-vector-icons/MaterialCommunityIcons"
+import styles from "./payment-method-styles"
 
-export const PaymentMethodScreen: FC<StackScreenProps<NavigatorParamList, "paymentMethod">> = observer(function PaymentMethodScreen() {
-  // Pull in one of our MST stores
-  // const { someStore, anotherStore } = useStores()
+export const PaymentMethodScreen: FC<StackScreenProps<NavigatorParamList, "paymentMethod">> =
+  observer(function PaymentMethodScreen() {
+    const [paymentMethod, setPaymentMethod] = useState("Credit Card")
 
-  // Pull in navigation via hook
-  // const navigation = useNavigation()
-  return (
-    <Screen preset="scroll">
-      <Text preset="header" text="paymentMethod" color="black" />
-    </Screen>
-  )
-})
+    return (
+      <Screen preset="scroll">
+        <View style={styles.headerContainer}>
+          <View style={styles.step}>
+            <Icon name="clipboard-list" size={28} color="#51C3FE" />
+            <Text preset="header" text="Review" color="#51C3FE" />
+          </View>
+          <View style={styles.dotSeperator} />
+          <View style={styles.step}>
+            <Icon name="wallet" size={28} color="#51C3FE" />
+            <Text preset="header" text="Payment Method" color="#51C3FE" />
+          </View>
+          <View
+            style={[
+              styles.dotSeperator,
+              {
+                borderColor: "gray",
+              },
+            ]}
+          />
+          <View style={styles.step}>
+            <Icon name="cash" size={28} color="gray" />
+            <Text preset="header" text="Payment" color="gray" />
+          </View>
+        </View>
+
+        <View style={styles.paymentMethod}>
+          <Text text="Payment Method" color="black" fontWeight="600" />
+
+          <TouchableOpacity style={[styles.paymentMethodCard, {
+            borderColor: paymentMethod === 'Credit Card' ? "#51C3FE" : "#e0e0e0"
+          }]} onPress={()=>setPaymentMethod('Credit Card')}>
+            <View style={styles.radioGroup}>
+              <Icon
+                name={paymentMethod === "Credit Card" ? "radiobox-marked" : "radiobox-blank"}
+                size={24}
+                color="#51C3FE"
+              />
+              <Text style={{ marginLeft: 8 }} text="Credit Card" color="black" fontWeight="600" />
+            </View>
+            <AutoImage
+              style={styles.creditCardImage}
+              source={require("../payment-method/VisaMasterCard.png")}
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={[styles.paymentMethodCard, {
+            borderColor: paymentMethod === 'Paypal' ? "#51C3FE" : "#e0e0e0"
+          }]} onPress={()=>setPaymentMethod('Paypal')}>
+            <View style={styles.radioGroup}>
+              <Icon
+                name={paymentMethod === "Paypal" ? "radiobox-marked" : "radiobox-blank"}
+                size={24}
+                color="#51C3FE"
+              />
+              <Text style={{ marginLeft: 8 }} text="Paypal" color="black" fontWeight="600" />
+            </View>
+            <AutoImage
+              style={styles.creditCardImage}
+              source={require("../payment-method/Paypal.png")}
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={[styles.paymentMethodCard, {
+            borderColor: paymentMethod === 'Apple Pay' ? "#51C3FE" : "#e0e0e0"
+          }]} onPress={()=>setPaymentMethod('Apple Pay')}>
+            <View style={styles.radioGroup}>
+              <Icon
+                name={paymentMethod === "Apple Pay" ? "radiobox-marked" : "radiobox-blank"}
+                size={24}
+                color="#51C3FE"
+              />
+              <Text style={{ marginLeft: 8 }} text="Apple Pay" color="black" fontWeight="600" />
+            </View>
+            <AutoImage
+              style={styles.creditCardImage}
+              source={require("../payment-method/ApplePay.png")}
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.paymentMethodFooter}>
+          <View>
+            <Text text="Payment Date" color="black" fontWeight="bold" />
+            <Text text="01/09/2020" color="black" fontWeight="600" />
+          </View>
+          <View>
+            <Text text="Price" color="black" fontWeight="bold" />
+            <Text text="$240" color="black" fontWeight="600" />
+          </View>
+          <View>
+            <Text text="Discount" color="black" fontWeight="bold" />
+            <View style={styles.discount}>
+            <Text text="-$120" color="black" fontWeight="600" />
+            <Text text=" (50%)" color="#51C3FE" fontWeight="600" />
+            </View>
+          </View>
+        </View>
+        <View style={styles.checkout}>
+          <Text text="Total Price" color="black" fontWeight="bold" />
+          <View style={[styles.dotSeperator, {
+            borderColor: "gray",
+          }]} />
+          <Text text="$120" color="black" fontWeight="600" />
+          <Button style={styles.checkoutButton}>
+            <Text text="Checkout" color="white" fontWeight="bold" />
+          </Button>
+        </View>
+      </Screen>
+    )
+  })
---
+++ app/screens/payment-method/payment-method-styles.tsx
@@ -0,0 +1,67 @@
+/* eslint-disable react-native/no-color-literals */
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    checkoutButton:{
+        backgroundColor: '#51C3FE',
+        borderRadius: 4,
+        height: 42,
+        width: 140,
+    },
+    creditCardImage:{
+        height: 24,
+        width: 100
+    },
+    discount:{
+        flexDirection: 'row',
+    },
+    dotSeperator:{
+        alignSelf: 'center',
+        borderColor: '#51C3FE',
+        borderRadius: 10,
+        borderStyle: 'dotted',
+        borderWidth: 2,
+        height: 1,
+        width: 80,
+    },
+    headerContainer:{
+        elevation: 0.5,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        paddingVertical: 20,
+    },
+    paymentMethod:{
+        marginHorizontal: 16,
+        marginTop: 20,
+    },
+    paymentMethodCard:{
+        borderColor: "#E0E0E0",
+        borderRadius: 4,
+        borderStyle: 'solid',
+        borderWidth: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 10,
+        padding: 16
+    },
+    paymentMethodFooter:{
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        marginTop: 260,
+        padding: 30,
+    },
+    radioGroup:{
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+    step:{
+        alignItems: 'center',
+    },
+    checkout:{
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+    }
+})
+
+export default styles
